22a5106d903d3025a0249e4411f8824af9d1cc72
disney <disney@tokeninsight.com>
2023-06-12 12:37:31 +0800

    add svg example

---
+++ src/components/SVG/SVG.tsx
@@ -0,0 +1,5 @@
+import ReactLogo from "../../assets/react.svg";
+
+export const SVGLogo = () => {
+  return <ReactLogo />;
+};
--- src/stories/SVG.stories.tsx
+++ src/stories/SVG.stories.tsx
@@ -1,19 +1,18 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import ReactLogo from "../assets/react.svg";
-import { Badge } from "../components/Badge/Badge";
+import { SVGLogo } from "../components/SVG/SVG";
 
-const meta: Meta<typeof Badge> = {
+const meta: Meta<typeof SVGLogo> = {
   /* 👇 The title prop is optional.
    * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
    * to learn how to generate automatic titles
    */
   title: "Components/SVG",
-  component: Badge,
+  component: SVGLogo,
 };
 
 export default meta;
 
-type Story = StoryObj<typeof Badge>;
+type Story = StoryObj<typeof SVGLogo>;
 
 /*
  *👇 Render functions are a framework specific feature to allow you control on how the component renders.
@@ -24,7 +23,7 @@ export const Primary: Story = {
   render: () => {
     return (
       <div style={{ display: "flex", gap: "10px" }}>
-        <ReactLogo />
+        <SVGLogo />
       </div>
     );
   },
