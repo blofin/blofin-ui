f60e543a98cfa33042d77c0e1c12b9fb57540778
disney <disney@tokeninsight.com>
2023-06-14 14:21:19 +0800

    fix prettier config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,9 @@
 // prettier.config.js
 module.exports = {
   tailwindFunctions: ["clsx", "cva"],
+  semi: true,
+  tabWidth: 2,
+  printWidth: 100,
+  trailingComma: "none",
+  jsxBracketSameLine: true
 };
