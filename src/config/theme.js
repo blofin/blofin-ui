8e2b43b507b4b2ae64fefa6a9c35b6cc6fe70654
disney <disney@tokeninsight.com>
2023-06-14 17:38:51 +0800

    sync color config

--- src/config/theme.js
+++ src/config/theme.js
@@ -3,7 +3,7 @@ module.exports = {
     display: ["group-hover"],
     boxShadow: {
       card: "0px 0px 24px rgba(240, 242, 250, 0.6)",
-      table: "inset 6px 0 8px -3px rgb(0 0 0 / 5%)",
+      table: "inset 6px 0 8px -3px rgb(0 0 0 / 5%)"
     },
     colors: {
       light: {
@@ -12,7 +12,7 @@ module.exports = {
           60: "rgba(47, 84, 235, 0.6)",
           40: "rgba(47, 84, 235, 0.4)",
           14: "rgba(47, 84, 235, 0.12)",
-          10: "rgba(47, 84, 235, 0.1)",
+          10: "rgba(47, 84, 235, 0.1)"
         },
         secondary: "#FF6600",
         success: "#0FCC71",
@@ -20,27 +20,27 @@ module.exports = {
         danger: "#D62035",
         green: {
           DEFAULT: "#08C787",
-          alpha: "rgba(8, 199, 135, 0.12)",
+          alpha: "rgba(8, 199, 135, 0.12)"
         },
         red: {
           DEFAULT: "#F2465D",
-          alpha: "rgba(242, 70, 93, 0.12)",
+          alpha: "rgba(242, 70, 93, 0.12)"
         },
         background: "#FFFFFF",
         label: {
-          DEFAULT: "rgba(10, 10, 10, 1)",
+          DEFAULT: "#0A0A0A",
           70: "rgba(10, 10, 10, 0.7)",
           40: "rgba(10, 10, 10, 0.4)",
-          20: "rgba(10, 10, 10, 0.2)",
+          20: "rgba(10, 10, 10, 0.2)"
         },
         fill: {
           primary: "#F7F8FA",
-          secondary: "#DCDEE5",
-          tertiary: "#ABADB2",
+          secondary: "#F0F1F5",
+          tertiary: "#ABADB2"
         },
         line: {
           primary: "#E6E8EB",
-          secondary: "#D2D3D6",
+          secondary: "#D2D3D6"
         },
         hover: {
           primary: "#3D63FF",
@@ -51,13 +51,13 @@ module.exports = {
           green: "#00D991",
           red: "#FF4F67",
           fill: {
-            primary: "#EDEDF5",
+            primary: "#F2F3F5",
             secondary: "#EBEDF2",
-            tertiary: "#C4C6CC",
-          },
+            tertiary: "#C4C6CC"
+          }
         },
         disabled: {
-          blue: "rgba(47, 84, 235, 0.4)",
+          blue: "rgba(47, 84, 235, 0.4)"
         },
         badge: {
           "primary-bg": "rgba(47, 84, 235, 0.12)",
@@ -65,20 +65,20 @@ module.exports = {
           "success-bg": "rgba(15, 204, 113, 0.12)",
           "warning-bg": "rgba(245, 205, 18, 0.12)",
           "danger-bg": "rgba(214, 32, 53, 0.12)",
-          "info-bg": "rgba(10, 10, 10, 0.14)",
+          "info-bg": "rgba(10, 10, 10, 0.14)"
         },
         pagination: {
           primary: "#2F54EB",
-          "primary-hover": "#F6F7FB",
+          "primary-hover": "#F6F7FB"
         },
         input: {
           primary: "#2F54EB",
           gray: "#DCDCDD",
-          error: "#D9242F",
+          error: "#D9242F"
         },
         header: {
-          bgColor: "#FFFFFF",
-        },
+          bgColor: "#FFFFFF"
+        }
       },
       dark: {
         primary: {
@@ -86,7 +86,7 @@ module.exports = {
           60: "rgba(92, 124, 255, 0.6)",
           40: "rgba(92, 124, 255, 0.4)",
           14: "rgba(92, 124, 255, 0.16)",
-          10: "rgba(92, 124, 255, 0.1)",
+          10: "rgba(92, 124, 255, 0.1)"
         },
         secondary: "#FF7D20",
         success: "#0FCC71",
@@ -94,27 +94,27 @@ module.exports = {
         danger: "#D62035",
         green: {
           DEFAULT: "#08C787",
-          alpha: "rgba(8, 199, 135, 0.14)",
+          alpha: "rgba(8, 199, 135, 0.14)"
         },
         red: {
           DEFAULT: "#F2465D",
-          alpha: "rgba(242, 70, 93, 0.14)",
+          alpha: "rgba(242, 70, 93, 0.14)"
         },
         background: "#010105",
         label: {
-          DEFAULT: "rgba(252, 253, 255, 1)",
-          70: "rgba(235, 236, 245, 0.7)",
+          DEFAULT: "#FCFDFF",
+          70: "rgba(235, 236, 245, 0.6)",
           40: "rgba(235, 236, 245, 0.4)",
-          20: "rgba(235, 236, 245, 0.2)",
+          20: "rgba(235, 236, 245, 0.2)"
         },
         fill: {
           primary: "#2F3033",
           secondary: "#606166",
-          tertiary: "#939499",
+          tertiary: "#939499"
         },
         line: {
           primary: "#2F3033",
-          secondary: "#47484D",
+          secondary: "#47484D"
         },
         hover: {
           primary: "#4869F0",
@@ -127,11 +127,11 @@ module.exports = {
           fill: {
             primary: "#47484D",
             secondary: "#787980",
-            tertiary: "#ACADB3",
-          },
+            tertiary: "#ACADB3"
+          }
         },
         disabled: {
-          blue: "rgba(92, 124, 255, 0.4)",
+          blue: "rgba(92, 124, 255, 0.4)"
         },
         badge: {
           "primary-bg": "rgba(92, 124, 255, 0.14)",
@@ -139,21 +139,21 @@ module.exports = {
           "success-bg": "rgba(15, 204, 113, 0.14)",
           "warning-bg": "rgba(245, 205, 18, 0.14)",
           "danger-bg": "rgba(214, 32, 53, 0.14)",
-          "info-bg": "rgba(235, 236, 245, 0.2)",
+          "info-bg": "rgba(235, 236, 245, 0.2)"
         },
         pagination: {
           primary: "#2F54EB",
-          "primary-hover": "#F6F7FB",
+          "primary-hover": "#F6F7FB"
         },
         input: {
           primary: "#2F54EB",
           gray: "#DCDCDD",
-          error: "#D9242F",
+          error: "#D9242F"
         },
         header: {
-          bgColor: "transparent",
-        },
-      },
-    },
-  },
+          bgColor: "transparent"
+        }
+      }
+    }
+  }
 };
