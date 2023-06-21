6a060b87deef2deb5d9309a263d8f4db8a8bd19a
helinyan <helinyan@tokeninsight.com>
2023-06-21 11:31:54 +0800

    feat:export useToast

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { Button } from "./components/Button/Button";
 import { Dialog } from "./components/Dialog/Dialog";
 import { Popover } from "./components/Popover/Popover";
 import { Typography } from "./components/Typography/Typography";
+import { useToast } from "./components/Toast/Toast";
 import "./scss/base.scss";
 import { BUIComponentColor, BUIComponentSize, BUITheme } from "./types/component";
 
@@ -12,3 +13,5 @@ export { Badge, Button, Dialog, Popover, Typography };
 export type { BUIComponentColor, BUIComponentSize, BUITheme };
 
 export { ThemeProvider };
+
+export { useToast };
