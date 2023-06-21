60f1042dc593db1a9e57416a1df8513a2af647d2
helinyan <helinyan@tokeninsight.com>
2023-06-21 11:16:35 +0800

    feat:export ThemeProvider

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import { Button } from "./components/Button/Button";
 import { Dialog } from "./components/Dialog/Dialog";
 import { Popover } from "./components/Popover/Popover";
 import { Typography } from "./components/Typography/Typography";
-import "./scss/base.scss";
-import { BUITheme,BUIComponentSize,BUIComponentColor } from "./types/component";
+import { BUITheme, BUIComponentSize, BUIComponentColor } from "./types/component";
+import { ThemeProvider } from "@storybook/theming";
 
 export { Button, Popover, Typography, Badge, Dialog };
 
-export type {BUITheme,BUIComponentSize,BUIComponentColor}
+export type { BUITheme, BUIComponentSize, BUIComponentColor };
+
+export { ThemeProvider };
