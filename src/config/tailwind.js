95d5c8dcde5735282ad8af11e2a554ce3331b39b
disney <disney@tokeninsight.com>
2023-06-21 18:38:25 +0800

    fix config

--- src/config/tailwind.js
+++ src/config/tailwind.js
@@ -4,15 +4,5 @@ module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   darkMode: "class",
   theme: UITheme,
-  plugins: [],
-  purge: {
-    enabled: process.env.NODE_ENV === "production",
-    // classes that are generated dynamically, e.g. `rounded-${size}` and must
-    // be kept
-    safeList: [],
-    content: [
-      "./src/**/*.{tsx,ts,js,jsx}"
-      // etc.
-    ]
-  }
+  plugins: []
 };
