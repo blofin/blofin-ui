74040433d35e0c586cd1c3a8527cc71747e1c6fb
helinyan <helinyan@tokeninsight.com>
2023-08-08 14:24:39 +0800

    feat:change useAlign hooks

--- src/hooks/useAlign.ts
+++ src/hooks/useAlign.ts
@@ -1,42 +1,30 @@
 import { useLayoutEffect, useState } from "react";
 
 const useAlign = (target: HTMLDivElement | null) => {
-  const [offsetX, setOffsetX] = useState(0);
-  const [offsetY, setOffsetY] = useState(0);
-  const [clientWidth, setClientWidth] = useState(0);
-  const [clientHeight, setClientHeight] = useState(0);
-  const [offsetLeft, setOffsetLeft] = useState(0);
-  const [offsetRight, setOffsetRight] = useState(0);
+  const [offset, setOffset] = useState({
+    offsetX: 0,
+    offsetY: 0,
+    clientWidth: 0,
+    clientHeight: 0,
+    offsetLeft: 0,
+    offsetRight: 0
+  });
 
   useLayoutEffect(() => {
     const wrapper = target;
     if (wrapper) {
-      setOffsetX(wrapper.offsetLeft);
-      setOffsetLeft(wrapper.offsetLeft);
-      setOffsetY(wrapper.offsetTop);
-      setClientWidth(wrapper.clientWidth);
-      setClientHeight(wrapper.clientHeight);
-      setOffsetRight(document.body.clientWidth - wrapper.offsetLeft - wrapper.clientWidth);
+      setOffset({
+        offsetX: wrapper.offsetLeft,
+        offsetY: wrapper.offsetTop,
+        clientWidth: wrapper.clientWidth,
+        clientHeight: wrapper.clientHeight,
+        offsetLeft: wrapper.offsetLeft,
+        offsetRight: document.body.clientWidth - wrapper.offsetLeft - wrapper.clientWidth
+      });
     }
   }, [target]);
 
-  console.log( {
-    offsetX,
-    offsetY,
-    clientWidth,
-    clientHeight,
-    offsetLeft,
-    offsetRight
-  });
-
-  return {
-    offsetX,
-    offsetY,
-    clientWidth,
-    clientHeight,
-    offsetLeft,
-    offsetRight
-  };
+  return offset
 };
 
 export default useAlign;
