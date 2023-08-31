8244575c4860301fb931e3762bdcbe420ec92b59
disney <disney@tokeninsight.com>
2023-08-31 15:10:49 +0800

    fix outline

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -52,7 +52,7 @@ const TextField = forwardRef<HTMLInputElement, InputBaseProps>((props, ref) => {
           {...otherProps}
           ref={ref as LegacyRef<HTMLInputElement>}
           className={cn(
-            "bu-h-full bu-w-full bu-flex-1 bu-bg-transparent focus-visible:bu-outline-0",
+            "bu-h-full bu-w-full bu-flex-1 bu-bg-transparent bu-outline-none focus:bu-outline-0 focus-visible:bu-outline-0",
             inputClassName,
             `${!startAdornment && "bu-pl-2"}`,
             `${!endAdornment && "bu-pr-2"}`
