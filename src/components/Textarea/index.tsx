f83b1a540d2928874b3035a1d62b7d26767cb8f9
helinyan <1043817056@qq.com>
2024-05-16 20:03:12 +0800

    textarea support row

--- src/components/Textarea/index.tsx
+++ src/components/Textarea/index.tsx
@@ -14,6 +14,7 @@ export interface TextareaProps extends React.InputHTMLAttributes<HTMLTextAreaEle
   error?: boolean;
   textareaClassName?: string;
   helperText?: string;
+  row?:number
 }
 
 const TextArea = forwardRef<HTMLTextAreaElement, TextareaProps>((props, ref) => {
@@ -27,6 +28,7 @@ const TextArea = forwardRef<HTMLTextAreaElement, TextareaProps>((props, ref) => 
     error,
     theme: mode,
     helperText,
+    row=4,
     ...otherProps
   } = props;
 
@@ -50,7 +52,7 @@ const TextArea = forwardRef<HTMLTextAreaElement, TextareaProps>((props, ref) => 
           id={typeof label === "string" ? (id ? id : `bui-${label}`) : undefined}
           ref={ref as LegacyRef<HTMLTextAreaElement>}
           disabled={disabled}
-          rows={4}
+          rows={row}
           className={`${styles.hideScrollbar} ${textAreaStyles({ theme: mode ? mode : theme })}`}
           {...otherProps}></textarea>
         {endAdornment && (
