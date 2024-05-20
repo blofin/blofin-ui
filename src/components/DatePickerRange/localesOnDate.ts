677c512e3fb454f779e05376e56674518ad731fb
Harlan <>
2024-05-20 16:29:04 +0800

    add  locale

---
+++ src/components/DatePickerRange/localesOnDate.ts
@@ -0,0 +1,172 @@
+
+import enUS from "date-fns/locale/en-US";
+import zhTW from "date-fns/locale/zh-TW";
+import ko from "date-fns/locale/ko";
+import uk from "date-fns/locale/uk";
+import vi from "date-fns/locale/vi";
+import ru from "date-fns/locale/ru";
+import es from "date-fns/locale/es";
+import de from "date-fns/locale/de";
+import fr from "date-fns/locale/fr";
+import tr from "date-fns/locale/tr";
+import af from "date-fns/locale/af";
+import arDZ from "date-fns/locale/ar-DZ";
+import arEG from "date-fns/locale/ar-EG";
+import arMA from "date-fns/locale/ar-MA";
+import arSA from "date-fns/locale/ar-SA";
+import arTN from "date-fns/locale/ar-TN";
+import ar from "date-fns/locale/ar";
+import az from "date-fns/locale/az";
+import beTarask from "date-fns/locale/be-tarask";
+import be from "date-fns/locale/be";
+import bg from "date-fns/locale/bg";
+import bn from "date-fns/locale/bn";
+import bs from "date-fns/locale/bs";
+import ca from "date-fns/locale/ca";
+import cs from "date-fns/locale/cs";
+import cy from "date-fns/locale/cy";
+import da from "date-fns/locale/da";
+import deAT from "date-fns/locale/de-AT";
+import el from "date-fns/locale/el";
+import eo from "date-fns/locale/eo";
+import et from "date-fns/locale/et";
+import eu from "date-fns/locale/eu";
+import faIR from "date-fns/locale/fa-IR";
+import fi from "date-fns/locale/fi";
+import frCA from "date-fns/locale/fr-CA";
+import frCH from "date-fns/locale/fr-CH";
+import fy from "date-fns/locale/fy";
+import gd from "date-fns/locale/gd";
+import gl from "date-fns/locale/gl";
+import gu from "date-fns/locale/gu";
+import he from "date-fns/locale/he";
+import hi from "date-fns/locale/hi";
+import hr from "date-fns/locale/hr";
+import ht from "date-fns/locale/ht";
+import hu from "date-fns/locale/hu";
+import hy from "date-fns/locale/hy";
+import id from "date-fns/locale/id";
+import is from "date-fns/locale/is";
+import itCH from "date-fns/locale/it-CH";
+import it from "date-fns/locale/it";
+import jaHira from "date-fns/locale/ja-Hira";
+import ja from "date-fns/locale/ja";
+import ka from "date-fns/locale/ka";
+import kk from "date-fns/locale/kk";
+import km from "date-fns/locale/km";
+import kn from "date-fns/locale/kn";
+import lb from "date-fns/locale/lb";
+import lt from "date-fns/locale/lt";
+import lv from "date-fns/locale/lv";
+import mk from "date-fns/locale/mk";
+import mn from "date-fns/locale/mn";
+import ms from "date-fns/locale/ms";
+import mt from "date-fns/locale/mt";
+import nb from "date-fns/locale/nb";
+import nlBE from "date-fns/locale/nl-BE";
+import nl from "date-fns/locale/nl";
+import nn from "date-fns/locale/nn";
+import oc from "date-fns/locale/oc";
+import pl from "date-fns/locale/pl";
+import ptBR from "date-fns/locale/pt-BR";
+import pt from "date-fns/locale/pt";
+import ro from "date-fns/locale/ro";
+import sk from "date-fns/locale/sk";
+import sl from "date-fns/locale/sl";
+import sq from "date-fns/locale/sq";
+import srLatn from "date-fns/locale/sr-Latn";
+import sr from "date-fns/locale/sr";
+import sv from "date-fns/locale/sv";
+import ta from "date-fns/locale/ta";
+import te from "date-fns/locale/te";
+import th from "date-fns/locale/th";
+import ug from "date-fns/locale/ug";
+import uzCyrl from "date-fns/locale/uz-Cyrl";
+import uz from "date-fns/locale/uz";
+
+export const locales = {
+  en: enUS,
+  "zh-tw": zhTW,
+  "ko-kr": ko,
+  "uk-ua": uk,
+  vi,
+  ru,
+  es,
+  de,
+  fr,
+  tr,
+  af,
+  ar,
+  az,
+  "ar-dz": arDZ,
+  "ar-EG": arEG,
+  "ar-MA": arMA,
+  "ar-SA": arSA,
+  "ar-TN": arTN,
+  "be-tarask": beTarask,
+  be,
+  bg,
+  bn,
+  bs,
+  ca,
+  cs,
+  cy,
+  da,
+  "de-at":deAT,
+  el,
+  eo,
+  et,
+  eu,
+  "fa-ir": faIR,
+  fi,
+  "fr-ca": frCA,
+  "fr-ch": frCH,
+  fy,
+  gd,
+  gl,
+  gu,
+  he,
+  hi,
+  hr,
+  ht,
+  hu,
+  hy,
+  id,
+  is,
+  "it-ch": itCH,
+  it,
+  "ja-Hira": jaHira,
+  ja,
+  ka,
+  kk,
+  km,
+  kn,
+  lb,
+  lt,
+  lv,
+  mk,
+  mn,
+  ms,
+  mt,
+  nb,
+  "nl-be": nlBE,
+  nl,
+  nn,
+  oc,
+  pl,
+  "pt-br": ptBR,
+  pt,
+  ro,
+  sk,
+  sl,
+  sq,
+  "sr-latn": srLatn,
+  sr,
+  sv,
+  ta,
+  te,
+  th,
+  ug,
+  "uz-cyrl": uzCyrl,
+  uz,
+};
