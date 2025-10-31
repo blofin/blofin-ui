81b29558903d83adeca7a8f98c9f206b44211eb4
leo <1043817056@qq.com>
2025-10-31 17:50:55 +0800

    release:1.0.0-beta.13

--- src/components/Checkbox/styles.ts
+++ src/components/Checkbox/styles.ts
@@ -9,59 +9,6 @@ const CheckboxCheckedStyles = cva("bu-select-none bu-text-sm", {
   }
 });
 
-const CheckedMarkCheckedStyles = cva("bu-flex bu-items-center bu-justify-center bu-border", {
-  variants: {
-    theme: {
-      light: ["bu-border-light-primary bu-bg-light-primary after:bu-border-light-background"],
-      dark: ["bu-border-light-background bu-bg-light-background after:bu-border-dark-background"]
-    }
-  }
-});
-
-const CheckedMarkCheckedSizeStyles = cva(
-  "bu-h-[10px] bu-w-[10px] after:bu-border-l-0 after:bu-border-t-0",
-  {
-    variants: {
-      size: {
-        small: [
-          "bu-h-[10px] bu-w-[10px] bu-rounded-[2px] after:bu-h-[6px] after:bu-w-[4px] after:bu-border-b-[1.8px] after:bu-border-r-[1.8px]"
-        ],
-        medium: [
-          "bu-h-[18px] bu-w-[18px] bu-rounded-[3px] after:bu-h-[10px] after:bu-w-[8px] after:bu-border-b-[2px] after:bu-border-r-[2px]"
-        ]
-      }
-    }
-  }
-);
-
-const UncheckedMarkCheckedStyles = cva(
-  "bu-flex bu-h-[10px] bu-w-[10px] bu-items-center bu-justify-center bu-border",
-  {
-    variants: {
-      theme: {
-        light: [
-          "bu-border-light-label-40 after:bu-border-light-background hover:bu-border-light-primary"
-        ],
-        dark: [
-          "bu-border-dark-label-40  after:bu-border-dark-background hover:bu-border-light-background"
-        ]
-      }
-    }
-  }
-);
-
-const CheckMarkVariants = cva(
-  "bu-flex bu-h-[10px] bu-w-[10px] bu-items-center bu-justify-center bu-border bu-bg-transparent",
-  {
-    variants: {
-      theme: {
-        light: ["bu-border-light-label-40"],
-        dark: ["bu-border-dark-label-40"]
-      }
-    }
-  }
-);
-
 const UncheckedboxLabelVariants = cva("bu-select-none bu-text-sm", {
   variants: {
     theme: {
@@ -80,25 +27,18 @@ const CheckboxDisabledLabelVariants = cva("bu-select-none bu-text-sm", {
   }
 });
 
-const disabledMarkStyles = cva(
-  "bu-flex bu-h-[10px] bu-w-[10px] bu-items-center bu-justify-center bu-border",
-  {
-    variants: {
-      theme: {
-        light: ["bu-border-light-label-40 after:bu-border-light-label-40"],
-        dark: ["bu-border-dark-label-40  after:bu-border-dark-label-40"]
-      }
+const CheckboxContainerSizeStyles = cva("bu-flex bu-items-center bu-justify-center", {
+  variants: {
+    size: {
+      small: ["bu-h-[10px] bu-w-[10px]"],
+      medium: ["bu-h-[18px] bu-w-[18px]"]
     }
   }
-);
+});
 
 export {
-  CheckMarkVariants,
   UncheckedboxLabelVariants,
   CheckboxDisabledLabelVariants,
-  CheckedMarkCheckedStyles,
-  UncheckedMarkCheckedStyles,
   CheckboxCheckedStyles,
-  CheckedMarkCheckedSizeStyles,
-  disabledMarkStyles
+  CheckboxContainerSizeStyles
 };
