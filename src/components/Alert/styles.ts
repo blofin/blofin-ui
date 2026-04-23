d5e588b7ad77f7b58a3a8305bc89292fddc27171
Stark <stark.zhang@blofin.io>
2026-04-23 11:47:58 +0800

    feat: alert component color

--- src/components/Alert/styles.ts
+++ src/components/Alert/styles.ts
@@ -1,16 +1,13 @@
 import { cva } from "class-variance-authority";
-import { BUITheme } from "../..";
-
-import { AlertType } from "./Alert";
 
 export const alertBgVariants = cva(
-  "bu-flex bu-min-h-[36px] bu-w-full  bu-justify-between bu-rounded-[6px] bu-px-[12px] bu-py-[8px]",
+  "bu-flex bu-min-h-[36px] bu-w-full bu-justify-between bu-rounded-[6px] bu-px-[12px] bu-py-[8px]",
   {
     variants: {
       type: {
         doubt: "bu-bg-base-primary-14",
         success: "bu-bg-base-success-14",
-        warning: "bu-bg-base-primary-14",
+        warning: "bu-bg-base-warning-14",
         danger: "bu-bg-base-danger-14"
       },
       alignType: {
@@ -21,13 +18,16 @@ export const alertBgVariants = cva(
   }
 );
 
-export const alertIconVariants = cva("ltr:bu-mr-[8px] rtl:bu-ml-[8px] bu-text-[20px]", {
-  variants: {
-    type: {
-      doubt: "bu-text-base-primary",
-      success: "bu-text-base-success",
-      warning: "bu-text-base-primary",
-      danger: "bu-text-base-danger"
+export const alertIconVariants = cva(
+  "bu-text-[16px] bu-leading-[16px] ltr:bu-mr-[4px] rtl:bu-ml-[4px]",
+  {
+    variants: {
+      type: {
+        doubt: "bu-text-base-primary",
+        success: "bu-text-base-success",
+        warning: "bu-text-base-warning",
+        danger: "bu-text-base-danger"
+      }
     }
   }
-});
+);
